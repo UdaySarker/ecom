ac68d764ddaefe23409033460745b22b2029ff07
showmikkhan <showmikkhan8@gmail.com>
2022-01-06 23:11:52 +0600

    vai check koren

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,5 +1,5 @@
 @extends('frontend.layouts.master')
-@section('title','E-SHOP || HOME PAGE')
+@section('title','BoiBazar || HOME PAGE')
 @section('main-content')
 <!-- Slider Area -->
 <section class="hero-slider">
