c361a67769ffe36a159a1deee7725f89a9f62ec1
showmikkhan <showmikkhan8@gmail.com>
2022-01-19 16:31:22 +0600

    Update head.blade.php

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Custom fonts for this template-->
     <link rel="stylesheet" href="{{asset('backend/css/sb-admin-2.css')}}">
-    <link rel="stylesheet" href="{{asset('backend/css/sb-admin-2.min.css')}}">
+    <!--<link rel="stylesheet" href="{{asset('backend/css/sb-admin-2.min.css')}}">-->
     <link href="{{asset('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
