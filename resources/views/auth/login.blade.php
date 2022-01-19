beac9049ecdfffe18c66c2a5624c4b7ce4df2112
UdaySarker <sarker450@gmail.com>
2022-01-19 17:15:33 +0600

    login page image remove

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>E-SHOP || Login Page</title>
+  <title>BoiBazar|| Login Page</title>
   @include('backend.layouts.head')
 
 </head>
@@ -14,14 +14,14 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-xl-10 col-lg-12 col-md-9 mt-5">
+      <div class="col-xl-10 col-lg-6 col-md-6 mt-5">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
+              {{-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> --}}
+              <div class="col-md-6 offset-md-3">
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
@@ -43,7 +43,7 @@
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
-                    
+
                     </div>
                     <div class="form-group">
                         <div class="form-check">
@@ -59,7 +59,7 @@
                     </button>
                   </form>
                   <hr>
-                   
+
                   <div class="text-center">
                     @if (Route::has('password.request'))
                         <a class="btn btn-link small" href="{{ route('password.request') }}">
