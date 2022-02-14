37ee69fb1fff2c9f8dee099a87d9f64a91b3a91a
showmikkhan <showmikkhan8@gmail.com>
2022-02-14 20:41:55 +0600

    Update header.blade.php
    change header structure

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -3,21 +3,7 @@
     <div class="topbar" style="background-color: #faebd7">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 col-md-12 col-12">
-                    <!-- Top Left -->
-                    <div class="top-left">
-                        <ul class="list-main">
-                            @php
-                                $settings=DB::table('settings')->get();
-
-                            @endphp
-                            <li><i class="ti-headphone-alt"></i>@foreach($settings as $data) {{$data->phone}} @endforeach</li>
-                            <li><i class="ti-email"></i> @foreach($settings as $data) {{$data->email}} @endforeach</li>
-                        </ul>
-                    </div>
-                    <!--/ End Top Left -->
-                </div>
-                <div class="col-lg-6 col-md-12 col-12">
+                <div class="col-lg-12 col-md-12 col-12">
                     <!-- Top Right -->
                     <div class="right-content">
                         <ul class="list-main">
